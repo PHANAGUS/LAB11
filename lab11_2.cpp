877cc0b904006b8d3fb3933d45f2f6d727ccdeb2
PHANAGUS <phanarin03072002@gmail.com>
2021-12-17 16:33:28 +0700

    lab11_2.cpp, cheerbook_copy.txt completed

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,13 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string t;
+	dest << "-------------------- BOOM ---------------------\n";
+	while(getline(source,t))
+	{
+		dest << t << endl;
+	}
+	dest << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
