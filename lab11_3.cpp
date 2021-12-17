8de50a7d187843ae1e2c6c3a0e671f576c462e71
PHANAGUS <phanarin03072002@gmail.com>
2021-12-17 16:52:13 +0700

    lab11_3.cpp completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    ifstream source;
+    source.open("score.txt");
+
+    string t;
+    int N=0;
+    double sum=0, sumpow=0, mean, sd;
+    while(getline(source,t))
+    {
+        sum += atof(t.c_str());
+        sumpow += pow(atof(t.c_str()),2);
+        N++;
+    }
+    mean = sum/N;
+    sd = sqrt((sumpow/N)-pow(mean,2));
+
+    cout << "Number of data = " << N << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd << endl;
+}
